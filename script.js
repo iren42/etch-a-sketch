82198d9db8cee4d0b23e3ae52b6684b1247d4ad9
iren42 <isabelleren618@gmail.com>
2025-01-26 16:53:13 +0100

    feat: erase grid on button click

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const NUMBEROFROWS = 16;
-const NUMBEROFCOL = 16;
+const MAXSIZE = 100;
+const DEFAULTSIZE = 16;
 
+const	resizeBtn = document.querySelector("#resize");
 const	body = document.querySelector("body");
-const container = document.createElement("div");
-container.classList.add("container");
 
-body.append(container);
-
-function createGrid()
+function createGrid(size = DEFAULTSIZE)
 {
+	const container = document.createElement("div");
+	container.classList.add("container");
+
+	body.append(container);
 	let	i = 0;
 	let	j = 0;
-	while (i++ < NUMBEROFROWS)
+	while (i++ < size)
 	{
 		const	row = document.createElement("div");
 		row.classList.add("row");
 		container.append(row);
+
 		j = 0;
-		while (j++ < NUMBEROFCOL)
+		while (j++ < size)
 		{
 			const	col = document.createElement("div");
 			col.classList.add("square");
@@ -29,4 +31,21 @@ function createGrid()
 		}
 	}
 }
+
+function eraseGrid()
+{
+	const container = document.querySelector(".container");
+	if (container.parentNode)
+	{
+		container.parentNode.removeChild(container);
+		console.log("container successfully removed");
+	}
+}
+
+resizeBtn.addEventListener("click", event => {
+	let	size = prompt("Enter new dimensions: ", "16");
+	eraseGrid();
+	createGrid(size);
+});
+
 createGrid();
