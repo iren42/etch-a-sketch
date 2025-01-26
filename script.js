ff39621b8c257a7b11ad217b411a1a2a78f1cee8
iren42 <isabelleren618@gmail.com>
2025-01-26 16:10:04 +0100

    feat: display grid with flexbox

---
+++ script.js
@@ -0,0 +1,29 @@
+const NUMBEROFROWS = 16;
+const NUMBEROFCOL = 16;
+
+const	body = document.querySelector("body");
+const container = document.createElement("div");
+container.classList.add("container");
+
+body.append(container);
+
+function createGrid()
+{
+	let	i = 0;
+	let	j = 0;
+	while (i++ < NUMBEROFROWS)
+	{
+		const	row = document.createElement("div");
+		row.classList.add("row");
+		container.append(row);
+		j = 0;
+		while (j++ < NUMBEROFCOL)
+		{
+			const	col = document.createElement("div");
+			col.classList.add("square");
+			row.append(col);
+		}
+	}
+}
+
+createGrid();
